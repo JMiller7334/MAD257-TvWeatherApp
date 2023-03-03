a03042330484a0870b39ffc9d5f29b8e534b7089
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-03-03 13:57:26 -0600

    Initial Commit

---
+++ MAD257-TvAppUITests/MAD257_TvAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MAD257_TvAppUITestsLaunchTests.swift
+//  MAD257-TvAppUITests
+//
+//  Created by student on 3/3/23.
+//
+
+import XCTest
+
+final class MAD257_TvAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
